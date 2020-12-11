37de3597a7e3c9532c27860997f8f3d9af9f8886
Moderno <71466911+Moderno858@users.noreply.github.com>
2020-12-10 21:50:13 -0300

    Update Ddos.py

--- Ddos.py
+++ Ddos.py
@@ -1,42 +1,38 @@
-  GNU nano 2.9.3                       ddos.py                        Modified  
-
-import  socket
+import socket
 import sys
 import os
 
-main = """
-____  ____   ___  ____  
-|  _ \|  _ \ / _ \/ ___| 
-| | | | | | | | | \___ \ 
-| |_| | |_| | |_| |___) |
-|____/|____/ \___/|____/ 
+main = ""
+"
 
-Powered by Moderno \n\n"""
+Powered by Moderno\ n\ n ""
+"
 count = 0
 
-def init(ip, port , main):
-           client  = socket.socket(socket.AF_INET, socket,SOCK_STREAM)
-           client.settimeout(0.03)
-           code = client.connect_ex((ip, int(port)))
-            if code == 0;
-                     print("[==>] - Realizando ataque ")
-                     print("IP: %s, PORT:%s",ip ,port)
-            else:
-              '      print("Servido idisponivel ou porta fechada\n")
-                    
-if len(sys.argv)< 5:
-print ('\n\n')
+def init(ip, port, main):
+    client = socket.socket(socket.AF_INET, socket, SOCK_STREAM)
+client.settimeout(0.03)
+code = client.connect_ex((ip, int(port)))
+if code == 0;
+print("[==>] - Realizando ataque ")
+print("IP: %s, PORT:%s", ip, port)
+else :
+    '      print("Servido idisponivel ou porta fechada\n")
+
+if len(sys.argv) < 5:
+    print('\n\n')
 print("Modo de uso:")
 print("Exe: root@localhost~# python DDOS.py 192.168.1.1 80")
 print('\n\n')
 sys.exit(0)
 
-else:
+else :
 
-ip = sys.argv[1]
+    ip = sys.argv[1]
 port = sys.argv[2]
 quantia = int(sys.argv[3])
-while count  < quantia:
-          count +=1
-        init(ip, port , main)
-print([!]"DDOS PARADO!")
+while count < quantia:
+    count += 1
+init(ip, port, main)
+print([!]
+    "DDOS PARADO!")
